e78f7d1937ea2d67dbf18bca3b433bdf4340034c
ElisabethFox <lisa.black.fox17@gmail.com>
2023-08-24 22:01:09 +0300

    add main components and styles

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -1,7 +1,12 @@
 const AppCard = () => {
     return (
         <div className="card">
+            <div className="users__container">
+            </div>
 
+            <div className="post__container">
+
+            </div>
         </div>
     );
 }
