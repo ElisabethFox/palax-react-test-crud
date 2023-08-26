ccb43380eedd514346727763f4537e44af066996
ElisabethFox <lisa.black.fox17@gmail.com>
2023-08-26 11:52:31 +0300

    add modals components

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -4,9 +4,21 @@ import UsersContainer from "./users/UsersContainer";
 
 const AppCard = () => {
     return (
+        <div>
         <div className="card">
+            <div className="card__headers">
+            <div className="users__header">
+                <h3>USERNAMES</h3>
+            </div>
+            <div className="posts__header">
+                <h3>POSTS</h3>
+            </div>
+        </div>
+        <div  className="card-containers">
             <UsersContainer />
             <PostsContainer />
+        </div>
+        </div>
         </div>
     );
 }
